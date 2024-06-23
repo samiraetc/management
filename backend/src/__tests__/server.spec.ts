a9e7ab052b7c797764f36d18f5691177b143e6d5
Samira Costa <samiramanuelac@gmail.com>
2024-06-23 19:31:46 +0100

    test: changed server spec

--- backend/src/__tests__/server.spec.ts
+++ backend/src/__tests__/server.spec.ts
@@ -1,20 +1,20 @@
+import {describe, expect, beforeAll, afterAll, it} from '@jest/globals';
+
 import { server } from '../server';
 
 describe('GET /', () => {
   beforeAll(async () => {
-    // Garanta que o servidor Fastify esteja escutando antes dos testes
     await server.listen({ port: 4001, host: '0.0.0.0' });
   });
 
   afterAll(async () => {
-    // Fecha o servidor Fastify após os testes
     await server.close();
   });
 
   it('responds with hello world', async () => {
     const response = await server.inject({
       method: 'GET',
-      url: '/'
+      url: '/api'
     });
 
     expect(response.statusCode).toBe(200);
