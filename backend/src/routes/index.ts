b6986f5bd22cf45f0589acfa8e8b1d3608c026a4
Samira Costa <samiramanuelac@gmail.com>
2024-06-23 17:16:05 +0100

    feat: resolve lint problems

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,11 +2,11 @@ import { FastifyInstance } from 'fastify';
 
 import authRoutes from './authRoutes';
 import labelRoutes from './labelRoutes';
+import priorityRoutes from './priorityRoutes';
 import userRoutes from './userRoutes';
 import workspaceLabelsRouters from './workspaceLabelsRoutes';
 import workspaceMembersRouters from './workspaceMembersRoutes';
 import workspaceRouters from './workspaceRoutes';
-import priorityRoutes from './priorityRoutes';
 
 const routes = async (app: FastifyInstance) => {
   app.register(authRoutes);
@@ -15,7 +15,7 @@ const routes = async (app: FastifyInstance) => {
   app.register(workspaceRouters);
   app.register(workspaceLabelsRouters);
   app.register(workspaceMembersRouters);
-  app.register(priorityRoutes)
+  app.register(priorityRoutes);
 };
 
 export default routes;
