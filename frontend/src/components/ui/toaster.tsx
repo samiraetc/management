307e219ef350cd0ca4f750cf9c684be6dff3aebf
Samira Costa <samiramanuelac@gmail.com>
2024-08-03 00:31:58 +0100

    feat: added padding in toast description when has icon

--- frontend/src/components/ui/toaster.tsx
+++ frontend/src/components/ui/toaster.tsx
@@ -7,6 +7,7 @@ import {
   ToastViewport,
 } from '@/components/ui/toast';
 import { useToast } from '@/components/ui/use-toast';
+import { cn } from '@/lib/utils';
 
 export function Toaster() {
   const { toasts } = useToast();
@@ -24,14 +25,14 @@ export function Toaster() {
         return (
           <Toast key={id} {...props}>
             <div className="grid gap-1">
-              <div className="flex items-center gap-1">
+              <div className="flex items-center gap-2">
                 {icon && icon}
                 {title && (
                   <ToastTitle className="font-medium">{title}</ToastTitle>
                 )}
               </div>
               {description && (
-                <ToastDescription>{description}</ToastDescription>
+                <ToastDescription className={cn(icon && 'pl-7')}>{description}</ToastDescription>
               )}
             </div>
             {action}
