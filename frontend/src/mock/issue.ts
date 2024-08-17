7e76b716936f494408c85aea4bdb153c2bf69fa4
Samira Costa <samiramanuelac@gmail.com>
2024-08-17 10:53:50 +0200

    feat: created issue mock

---
+++ frontend/src/mock/issue.ts
@@ -0,0 +1,55 @@
+export const issue = {
+  id: '4152cfcc-9be4-4a32-b403-1ecb5aaae3e0',
+  title: 'Welcome 👋🏼',
+  labels: [
+    {
+      name: 'coffe',
+      color: '#644536',
+      id: '',
+      can_edit: false,
+      created_at: '2024-07-31T21:35:08.371Z',
+    },
+  ],
+  description: `<p>Welcome to my new project</p><pre><code class="language-javascript">{
+  id: '4152cfcc-9be4-4a32-b403-1ecb5aaae3e0',
+  title: 'Welcome 👋🏼',
+  labels: [
+    {
+      name: 'coffe',
+      color: '#644536',
+    },
+  ],
+  description:
+    '&lt;p&gt;New Description&lt;/p&gt;',
+  estimative: 16,
+  priority: 'high',
+  status: 'to_do'
+}</code></pre>`,
+  identifier: 'TSK-1',
+  estimative: 16,
+  priority: 'high',
+  status: 'to_do',
+  due_date: '2024-07-31T21:35:08.371Z',
+  created_at: '2024-02-19T15:56:37.408Z',
+  updated_at: '2024-07-07T09:54:24.614Z',
+  assigned_to: {
+    full_name: 'Samira Costa',
+    username: 'samiramanuelac',
+  },
+};
+
+// {
+//   id: '4152cfcc-9be4-4a32-b403-1ecb5aaae3e0',
+//   title: 'Welcome 👋🏼',
+//   labels: [
+//     {
+//       name: 'coffe',
+//       color: '#644536',
+//     },
+//   ],
+//   description:
+//     '<p>New Description</p>',
+//   estimative: 16,
+//   priority: 'high',
+//   status: 'to_do'
+// }
