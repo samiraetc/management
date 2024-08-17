435d8f474979c82e430a6fcc782d40a10a048f78
Samira Costa <samiramanuelac@gmail.com>
2024-08-17 11:07:31 +0200

    feat: created task properties component to show status, priority and estimative with differente layout in desktop and mobile

---
+++ frontend/src/components/CreateTask/CreateTaskProperties.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Status from '../Status/Status';
+import Priority from '../Priority/Priority';
+import { TaskPriority } from '@/lib/utils';
+import Estimative from '../Estimative/Estimative';
+import { issue } from '@/mock/issue';
+
+const CreateTaskProperties = () => {
+  return (
+    <>
+      <Status
+        status={'backlog'}
+        task={issue}
+        label
+        className="h-7 w-full rounded-md border px-3 font-normal"
+      />
+
+      <Priority
+        priority={'none' as TaskPriority}
+        task={issue}
+        className="h-7 w-full rounded-md border px-3 font-normal"
+        label
+      />
+      <Estimative
+        estimative={null}
+        task={issue}
+        className="flex h-7 w-full items-center rounded-md border px-3 font-normal"
+      />
+    </>
+  );
+};
+
+export default CreateTaskProperties;
