009014b3c6fa3ccdb798a5e751b510fcafe79303
Samira Costa <samiramanuelac@gmail.com>
2024-08-17 11:05:28 +0200

    feat: changed priority to show label

--- frontend/src/components/Priority/Priority.tsx
+++ frontend/src/components/Priority/Priority.tsx
@@ -1,5 +1,5 @@
-import { getPriorityProps, TaskPriority } from '@/lib/utils';
-import React from 'react';
+import { cn, getPriorityProps, TaskPriority } from '@/lib/utils';
+import React, { useState } from 'react';
 import {
   Command,
   CommandEmpty,
@@ -18,7 +18,9 @@ import { Check } from 'lucide-react';
 
 interface IPriority {
   priority: TaskPriority;
-  taskId: string;
+  task: Task;
+  label?: boolean;
+  className?: string;
 }
 
 export const priorities = [
@@ -44,40 +46,53 @@ export const priorities = [
   },
 ];
 
-const Priority = ({ priority }: IPriority) => {
+const Priority = ({ priority, label, className }: IPriority) => {
+  const [value, setValue] = useState<TaskPriority>(priority);
+  const [open, setOpen] = useState<boolean>(false);
   return (
-    <div className="ml-2 w-4 sm:ml-0">
-      <DropdownMenu>
-        <DropdownMenuTrigger>
-          {getPriorityProps(priority).icon}
+    <div className={!label ? 'ml-2 w-4 sm:ml-0' : ''}>
+      <DropdownMenu open={open} onOpenChange={setOpen}>
+        <DropdownMenuTrigger
+          onClick={() => setOpen(true)}
+          className={cn(
+            label &&
+              'flex h-8 w-48 items-center gap-2 p-1 text-xs font-medium text-foreground hover:rounded-md hover:bg-muted/70',
+            className,
+          )}
+        >
+          <span>{getPriorityProps(value).icon}</span>
+          {label && <p>{getPriorityProps(value).label}</p>}
         </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <Command className="w-60 text-gray-700">
+        <DropdownMenuContent align="start" side="left" className="w-56">
+          <Command className="w-full text-gray-700">
             <CommandInput
               placeholder="Change priority..."
               autoFocus
-              className="text-base"
+              className="text-sm"
             />
 
             <CommandList className="p-1">
               <CommandEmpty>No results found.</CommandEmpty>
 
               {priorities.map((priori, index) => {
-                const { icon, label } = getPriorityProps(
-                  priori.key as TaskPriority,
-                );
+                const key = priori.key as TaskPriority;
+                const { icon, label } = getPriorityProps(key);
                 return (
                   <CommandItem
                     className="cursor-pointer px-4 py-1.5"
                     key={index}
+                    onSelect={() => {
+                      setValue(key);
+                      setOpen(false);
+                    }}
                   >
                     <div className="flex w-full items-center justify-between">
                       <div className="flex items-center gap-4">
                         {icon}
-                        <p className="text-base"> {label}</p>
+                        <p className="text-sm"> {label}</p>
                       </div>
 
-                      {priority === priori.key && (
+                      {value === priori.key && (
                         <Check width={20} height={20} className="mr-2" />
                       )}
                     </div>
