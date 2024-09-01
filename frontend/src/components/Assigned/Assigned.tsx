f68c9169740d2c6bebfacc1dfc4e23a63f361c3d
Samira Costa <samiramanuelac@gmail.com>
2024-09-01 20:31:05 +0200

    feat: changed assigned to popover and only request data when click

--- frontend/src/components/Assigned/Assigned.tsx
+++ frontend/src/components/Assigned/Assigned.tsx
@@ -1,5 +1,5 @@
 import { cn } from '@/lib/utils';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import {
   Command,
   CommandDialog,
@@ -10,10 +10,10 @@ import {
 } from '@/components/ui/command';
 
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
 import { Check, CircleUserRound } from 'lucide-react';
 import useWindowSize from '@/hook/useWindowSize/useWindowSize';
 import { updateTaskDetails } from '@/services/Task/taskService';
@@ -30,13 +30,13 @@ interface IAssigned {
   label?: boolean;
 }
 
-const UserList = ({
-  member,
+const AssignedItem = ({
+  members,
   value,
   setValue,
   onClose,
 }: {
-  member: User[];
+  members: User[];
   value: User | null;
   setValue: (selected: User | null) => void;
   onClose: () => void;
@@ -68,25 +68,25 @@ const UserList = ({
           {value === null && <Check width={20} height={20} className="mr-2" />}
         </div>
       </CommandItem>
-      {member.map((user, index) => {
+      {members.map((member, index) => {
         return (
           <CommandItem
             className="cursor-pointer px-4 py-1.5"
             key={index}
             onSelect={() => {
-              setValue(user);
+              setValue(member);
               onClose();
             }}
           >
             <div className="flex w-full items-center justify-between">
               <div className="flex items-center justify-between gap-4">
-                {user.image ? (
+                {member.image ? (
                   <Avatar className="size-[1.35rem]">
                     <AvatarImage
                       width={12}
                       height={12}
-                      src={user.image as string}
-                      alt="Profile"
+                      src={member.image as string}
+                      alt="profile icon"
                       className="flex rounded-full object-cover"
                     />
                   </Avatar>
@@ -94,10 +94,10 @@ const UserList = ({
                   <CircleUserRound width={22} />
                 )}
 
-                <p className="text-sm"> {user.full_name}</p>
+                <p className="text-sm">{member.full_name}</p>
               </div>
 
-              {value?.id === user.id && (
+              {value?.id === member.id && (
                 <Check width={20} height={20} className="mr-2" />
               )}
             </div>
@@ -115,25 +115,28 @@ const Assigned = ({
   setProperties,
   teamId,
 }: IAssigned) => {
-  const [value, setValue] = useState<User | null>(assigned);
-  const [open, setOpen] = useState<boolean>(false);
-  const [members, setMembers] = useState<User[]>([]);
+  const [currentAssigned, setCurrentAssigned] = useState<User | null>(assigned);
+  const [openPopover, setPopoverOpen] = useState<boolean>(false);
+  const [teamMembers, setTeamMembers] = useState<User[]>([]);
   const [openDialog, setOpenDialog] = useState<boolean>(false);
   const isMobile = useWindowSize();
 
-  useEffect(() => {
-    const handleGetWorkspaceUsers = async () => {
-      return await getTeamMembers(teamId ?? '').then((response) => {
-        setMembers(response);
-      });
-    };
-    handleGetWorkspaceUsers();
-  }, []);
+  const getAllTeamMembers = async () => {
+    if (!openDialog && !openPopover) {
+      return await getTeamMembers(teamId ?? '')
+        .then((response) => {
+          setTeamMembers(response);
+        })
+        .finally(() => (isMobile ? setOpenDialog(true) : setPopoverOpen(true)));
+    } else {
+      isMobile ? setOpenDialog(!openDialog) : setPopoverOpen(!open);
+    }
+  };
 
   const handleSetValue = async (value: User | null) => {
-    setValue(value);
+    setCurrentAssigned(value);
     setProperties
-      ? setProperties(value)
+      ? setProperties(value ?? assigned)
       : await updateTaskDetails(task?.id ?? '', {
           assigned: value ? value.id : null,
         });
@@ -141,54 +144,54 @@ const Assigned = ({
 
   return (
     <div>
-      <DropdownMenu open={open} onOpenChange={setOpen}>
-        <DropdownMenuTrigger
-          onClick={() => (isMobile ? setOpenDialog(true) : setOpen(true))}
+      <Popover open={openPopover} onOpenChange={setPopoverOpen}>
+        <PopoverTrigger
+          onClick={() => getAllTeamMembers()}
           className={cn(
-            'outline-none flex h-8 w-48 items-center text-stone-600 gap-4 p-1 text-xs font-medium text-foreground hover:rounded-md hover:bg-muted/70',
+            'flex h-8 w-48 items-center gap-4 p-1 text-xs font-medium text-foreground text-stone-600 outline-none hover:rounded-md hover:bg-muted/70',
             className,
           )}
         >
           <div className="flex items-center gap-2">
-            {value?.id ? (
+            {currentAssigned?.id ? (
               <>
-                {value.image ? (
+                {currentAssigned.image ? (
                   <Avatar className="size-4">
                     <AvatarImage
                       width={12}
                       height={12}
-                      src={value.image as string}
+                      src={currentAssigned.image as string}
                       alt="Profile"
                       className="flex rounded-full object-cover"
                     />
                   </Avatar>
                 ) : (
-                  <CircleUserRound width={16} className='text-stone-600' />
+                  <CircleUserRound width={16} className="text-stone-600" />
                 )}
-                <p className='text-stone-600'>{value.full_name}</p>
+                <p className="text-stone-600">{currentAssigned.full_name}</p>
               </>
             ) : (
               <>
-                <CircleUserRound width={16} className='text-stone-600' />
-                <p className='text-stone-600'>Assigned</p>
+                <CircleUserRound width={16} className="text-stone-600" />
+                <p className="text-stone-600">Assigned</p>
               </>
             )}
           </div>
-        </DropdownMenuTrigger>
+        </PopoverTrigger>
 
         {!isMobile && (
-          <DropdownMenuContent className="w-56" align="center" side="bottom">
+          <PopoverContent className="w-56 p-1" align="center" side="bottom">
             <Command className="w-full text-gray-700">
-              <UserList
-                member={members}
-                value={value}
+              <AssignedItem
+                members={teamMembers}
+                value={currentAssigned}
                 setValue={handleSetValue}
-                onClose={() => setOpen(false)}
+                onClose={() => setPopoverOpen(false)}
               />
             </Command>
-          </DropdownMenuContent>
+          </PopoverContent>
         )}
-      </DropdownMenu>
+      </Popover>
 
       {isMobile && (
         <CommandDialog
@@ -200,9 +203,9 @@ const Assigned = ({
           <div className="mb-2 mt-3 px-3">
             <p className="max-w-full truncate rounded-sm border bg-muted p-1 text-xs text-muted-foreground">{`${task?.identifier} - ${task?.title}`}</p>
           </div>
-          <UserList
-            member={members}
-            value={value}
+          <AssignedItem
+            members={teamMembers}
+            value={currentAssigned}
             setValue={handleSetValue}
             onClose={() => setOpenDialog(false)}
           />
